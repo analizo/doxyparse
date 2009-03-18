2221e2cf7a19927915ae7df09a1c10d18b00f3dc
Joenio Costa <joenio@perl.org.br>
2009-03-18 00:41:38 -0300

    loop over files to be parsed

--- src/doxyparse.cpp
+++ src/doxyparse.cpp
@@ -1,5 +1,21 @@
 #include <doxyparse.h>
+#include "doxygen.h"
+#include "message.h"
+
+static StringList g_inputFiles;         
 
 int main(int argc, char** argv) {
+  initDoxygen();
+  readConfiguration(argc,argv);
+  checkConfiguration();
+  adjustConfiguration();
+  searchInputFiles(g_inputFiles);
+  QCString *s=g_inputFiles.first();
+  while (s) {
+    QCString fileName=*s;
+    msg("Parsing file %s...\n", fileName.data());
+    // ... parse file!
+    s=g_inputFiles.next();
+  }
   return 0;
 }
