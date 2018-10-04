37a366c6ac1a7339685f19fccea7b7a7ed6fff04
Alax Alves <alaxallves@gmail.com>
2018-10-04 16:53:32 -0300

    Creating release using GitHub API

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,47 @@
-#!bin/sh
+#!/bin/sh
+
+VERSION=$(cat VERSION)
+TEXT=$2
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+REPO_FULL_NAME=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
+TOKEN_SHA=$(cat auth-token)
+TOKEN=$(git config --global github.token $TOKEN_SHA)
 
 # Compiling
 cmake -G "Unix Makefiles" -Dbuild_parse=ON
 make
-make install
 
+# Port to build-package.sh
 # Generating tarball
-debuild -us -uc
+# tar cvf doxyparse-$VERSION.tar.gz *
+
+# Generating debian package
+# cd .. && dkpg-build doxyparse
+# dpkg-buildpackage -rfakeroot -d -us -uc
+
+# 1) Create a personal access token through GitHub UI, we only need "Repo" access
+# 2) Place it in your config by "git config --global github.token YOUR_TOKEN"
+# 3) Use bash script to make CURL request `sh git-release.sh "1.5.0" "Test description"`
+
+generate_post_data()
+{
+  cat <<EOF
+{
+  "tag_name": "$VERSION",
+  "target_commitish": "$BRANCH",
+  "name": "$VERSION",
+  "body": "$TEXT",
+  "draft": false,
+  "prerelease": false
+}
+EOF
+}
+
+echo "Create release $VERSION for repo: $REPO_FULL_NAME branch: $BRANCH"
+curl --data "$(generate_post_data)" "https://api.github.com/repos/$REPO_FULL_NAME/releases?access_token=$TOKEN"
+
+# Deploying to analizo.org
+# TODO: Refactor this
+git remote add analizo-org https://github.com/GCES2018-2/analizo.github.io.git
+git config --global push.default simple
+git push analizo-org testing-deploy
--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,9 +2,9 @@
 
 # Represents the latest version of the project according to VERSION file
 VERSION=$(cat VERSION)
-# TODO Refactor this
-DOCKERFILE_AMD64=/home/travis/build/GCES2018-2/doxyparse/Dockerfile-amd64
-DOCKERFILE_I386=/home/travis/build/GCES2018-2/doxyparse/Dockerfile-i386
+
+DOCKERFILE_AMD64=$PWD/Dockerfile-amd64
+DOCKERFILE_I386=$PWD/Dockerfile-i386
 
 echo "Latest Doxyparse version is $VERSION";
 
