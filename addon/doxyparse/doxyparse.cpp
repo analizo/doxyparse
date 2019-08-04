f80f348060b76e53f80899c5c3ad5aadca09292d
Joenio Costa <joenio@joenio.me>
2019-08-04 15:43:20 -0300

    Revert "new Doxyfile option: MODIFY_SPECIAL_CHARS"
    This reverts commit 5a71215565d00f78b13741f78df2dbf45678a1d1.

--- addon/doxyparse/doxyparse.cpp
+++ addon/doxyparse/doxyparse.cpp
@@ -425,7 +425,6 @@ int main(int argc,char **argv) {
 
   // check and finalize the configuration
   checkConfiguration();
-  Config_getBool(MODIFY_SPECIAL_CHARS)=FALSE;
   adjustConfiguration();
 
   // setup the non-default configuration options
